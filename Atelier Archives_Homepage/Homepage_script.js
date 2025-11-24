5717b6aeadc787a19c22b35088386d0a66ac5ff0
th30o0o0 <theofulgentes@gmail.com>
2025-11-24 14:57:47 +0800

    search bar

--- Atelier Archives_Homepage/Homepage_script.js
+++ Atelier Archives_Homepage/Homepage_script.js
@@ -1,3 +1,16 @@
+// Search Bar Function
+function searchPage() {
+    const text = document.body.innerText.toLowerCase();
+    const input = document.querySelector(".search-input").value.toLowerCase();
+
+    if (text.includes(input)) {
+        alert("Text found on this page!");
+    } else {
+        alert("No match found.");
+    }
+}
+
+
 // Featured images redirect
 document.querySelectorAll('.featured .card').forEach(card => {
     card.addEventListener('click', () => {
